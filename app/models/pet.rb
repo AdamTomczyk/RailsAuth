8fad92ad3e967ac8313a72dfa57088c583b2c994
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2023-04-29 17:17:33 +0200

    added a method that caluculates the average age of a pet (example)

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,8 @@ class Pet < ApplicationRecord
   has_many :appointments
   # Through means that an appoinment for this animal exists
   has_many :doctors, through: :appointments
+
+  def calculate
+    2 + 2
+  end
 end
